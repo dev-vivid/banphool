792c47a7f6df30b5f29af41f52328b8ac7b420a1
Mohammed Suhaibu (BTC-2025) <suhaibu@bontonsoftwares.com>
2026-08-06 15:35:08 +0530

    auth limiter

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -10,7 +10,7 @@ export const globalLimiter = rateLimit({
 });
 
 export const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 2 * 60 * 1000,
   max: 10,
   standardHeaders: true,
   legacyHeaders: false,
